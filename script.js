9e5edf88c2e37f737be7cf53f6795da0f610d905
Booligoosh <booligoosh@optusnet.com.au>
2017-07-02 13:32:22 +1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+var answer = '';
+function generateAnswer() {
+  answer = randomNum(0,9999).toString();
+  while(answer.length < 4) {
+	answer = '0' + answer;
+}
+function randomNum(x,y) {
+	return(Math.floor(Math.random() * y) + x);
+}
